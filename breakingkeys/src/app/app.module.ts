4f300af7316ebe0522ca4227a7dda66961e70c75
Ildar Sirazutdinov <sirazutdinov@lanit.ru>
2017-12-02 23:58:28 +0300

    WOAH, cool stuff, now with Angular ;P

---
+++ breakingkeys/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ breakingkeys/src/app/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
+
+@NgModule({
+  imports: [MatButtonModule, MatToolbarModule],
+  exports: [MatButtonModule, MatToolbarModule],
+})
+export class MaterialModule { }
